31990700460958eea2ad0f825bccd183cf4c0298
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-02-14 18:12:34 +0000

    imports fucked in amdev workspace

--- src/shipr/expresslink.py
+++ src/shipr/expresslink.py
@@ -1,10 +1,18 @@
 from zeep import Client
 
 # Initialize the client with the URL of your WSDL
-client = Client(wsdl='http://example.com/service?wsdl')
+settings = Settings(strict=False, xml_huge_tree=True)
+client = Client(wsdl=r'C:\Users\giles\prdev\am_dev\shipr\resources\ExpressLink Documentation\ShipService_v14.wsdl', settings=settings)
 
+...
+
+
+
+response = client.find(
+
+)
 # Access a specific service operation and call it
-response = client.service.YourOperationName(param1='value1', param2='value2')
+# response = client.service.YourOperationName(param1='value1', param2='value2')
 
 # Process the response
-print(response)
+# print(response)
