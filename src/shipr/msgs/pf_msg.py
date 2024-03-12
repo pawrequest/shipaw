d40d12a787ef2ff8e6cadb131cc94aa7e3a1beab
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-03-12 17:12:44 +0000

    dont raise for alert error

--- src/shipr/msgs/pf_msg.py
+++ src/shipr/msgs/pf_msg.py
@@ -40,7 +40,7 @@ class BaseResponse(pf_shared.BasePFType):
                     logger.warning(f'ExpressLink Warning: {alt.message} for {cls.__name__}')
                 elif alt.type == 'ERROR':
                     logger.error(f'ExpressLink Error: {alt.message} for {cls.__name__}')
-                    raise ValueError(f'ExpressLink Error: {alt.message} for {cls.__name__}')
+                    # raise ValueError(f'ExpressLink Error: {alt.message} for {cls.__name__}')
                 else:
                     logger.info(f'ExpressLink {alt.type}: {alt.message} for {cls.__name__}')
         return v
