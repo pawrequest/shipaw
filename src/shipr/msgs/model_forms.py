346f97f517981f72059f3629c754fd86de33fa8d
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-03-13 11:05:11 +0000

    modelforms

---
+++ src/shipr/msgs/model_forms.py
@@ -0,0 +1,19 @@
+from . import pf_msg
+
+working_models = [
+    pf_msg.PrintLabelRequest,
+    pf_msg.PrintDocumentRequest,
+    pf_msg.CreateManifestRequest,
+    pf_msg.PrintManifestRequest,
+    pf_msg.ReturnShipmentRequest,
+    pf_msg.CCReserveRequest,
+    pf_msg.CancelShipmentRequest
+
+]
+
+not_working_array_fields = [
+    pf_msg.CreatePrintRequest,
+    pf_msg.FindRequest,
+    pf_msg.CreateShipmentRequest,
+
+]
