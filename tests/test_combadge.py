b2bd7541b6ec32905b87cbb1831e721c2f23bd90
paw <pawrequest@users.noreply.github.com>
2024-04-23 03:22:37 +0100

    rm unneccessary test

--- tests/test_combadge.py
+++ tests/test_combadge.py
@@ -1,19 +1,7 @@
 from __future__ import annotations
 
-from combadge.support.zeep.backends.sync import ZeepBackend
-
 from shipaw import msgs
-from shipaw.models import PAF, pf_ext
-
-
-def test_find_paf(el_client):
-    service = ZeepBackend(el_client.service)[msgs.FindService]
-    paf = PAF(postcode='NW6 4TE')
-    req = msgs.FindRequest(authentication=el_client.settings.auth, paf=paf)
-    response = service.find(request=req)
-    assert isinstance(response, msgs.FindResponse)
-    assert isinstance(response.paf, PAF)
-    assert isinstance(response.paf.specified_neighbour[0].address[0], pf_ext.AddressRecipient)
+from shipaw.models import pf_ext
 
 
 def test_client_gets_candidates(el_client, address_r):
@@ -44,5 +32,3 @@ def test_client_sends_inbound(shipment, el_client, tmp_path):
 def check_label(el_client, resp, tmp_path):
     label = el_client.get_label(ship_num=resp.shipment_num, dl_path=tmp_path / 'tmp.pdf')
     assert label.exists()
-
-
