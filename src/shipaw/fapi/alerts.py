0cb75eda11ec59df8ebcfce6c146c9f302fda3a4
Paw <paw@pawrequest.com>
2025-10-24 18:35:19 +0100

    refactor royal mail alert logic for clarity and accuracy

--- src/shipaw/fapi/alerts.py
+++ src/shipaw/fapi/alerts.py
@@ -7,7 +7,6 @@ from pydantic import Field
 from shipaw.models.base import ShipawBaseModel
 from shipaw.models.ship_types import ShipDirection
 from shipaw.providers.provider_abc import ProviderName
-from shipaw.providers.registry import PROVIDER_REGISTER
 
 
 class AlertType(StrEnum):
@@ -97,11 +96,8 @@ async def maybe_alert_phone_number(phone_num: str):
 
 async def check_royal_mail(shipment_request) -> Alerts:
     alerts = Alerts.empty()
-    if ProviderName.ROYAL_MAIL in PROVIDER_REGISTER and shipment_request.provider_name == ProviderName.ROYAL_MAIL:
-        if shipment_request.shipment.service != 'NEXT_DAY':
-            msg = f'Royal Mail only supports NEXT_DAY service in this demo. You selected {shipment_request.shipment.service}.'
-            alerts += Alert(message=msg, type=AlertType.ERROR)
+    if shipment_request.provider_name == ProviderName.ROYAL_MAIL:
         if shipment_request.shipment.direction != ShipDirection.OUTBOUND:
-            msg = f'Royal Mail only supports OUTBOUND shipments in this demo. You selected {shipment_request.shipment.direction}.'
+            msg = f'Royal Mail only supports OUTBOUND shipments at this time. Please use another provider for {shipment_request.shipment.direction} shipments.'
             alerts += Alert(message=msg, type=AlertType.ERROR)
     return alerts
