2ef1d370beeb185edc17860fa9552c2f3a69de85
Paw <paw@pawrequest.com>
2025-11-06 17:22:40 +0000

    change log level from warning to info for ShipawSettings

--- src/shipaw/config.py
+++ src/shipaw/config.py
@@ -102,7 +102,7 @@ class ShipawSettings(BaseSettings):
 
     @model_validator(mode='after')
     def log_self(self):
-        logger.warning('ShipawSettings:\n' + pprint.pformat(self.model_dump(), indent=4))
+        logger.info('ShipawSettings:\n' + pprint.pformat(self.model_dump(), indent=4))
         return self
 
     ## SET LOGGING & LABELS ##
