a005d557966b668c5b7a3b200961f8c296cefc1c
Paw <paw@pawrequest.com>
2026-03-25 13:36:59 +0000

    feat: update address search test to utilize provider client and result dumping

--- tests/test_address_search.py
+++ tests/test_address_search.py
@@ -1,7 +1,14 @@
+from helpers import dump_result_model
+from shipaw import RoyalMailProvider
+from shipaw.config import SHIPAW_SETTINGS, populate_providers
 from shipaw.providers.registry import PROVIDER_REGISTER
 
 
-def test_address_search():
-    provider = PROVIDER_REGISTER.get('ROYAL_MAIL')
-    res = await provider.address_search(search_text)
-    return JSONResponse(res.dict())
+def test_address_search(sample_remote_address):
+    populate_providers()
+    search_text = sample_remote_address.search_string
+    provider: RoyalMailProvider = PROVIDER_REGISTER.get('ROYAL_MAIL')
+    res = provider.client.address_search(search_text)
+    dump_result_model(res)
+
+    ...
