f14e1bd8c3e8fe59050df4a91adf77253a100606
Paw <paw@pawrequest.com>
2026-03-26 15:15:57 +0000

    - permissive addressRecordDef bc rm swagger lies - include business name in address search

--- tests/test_address_search.py
+++ tests/test_address_search.py
@@ -6,9 +6,11 @@ from shipaw.providers.registry import PROVIDER_REGISTER
 
 def test_address_search(sample_remote_address):
     populate_providers()
-    search_text = sample_remote_address.search_string
+    # search_text = sample_remote_address.search_string
+    search_text = '80 East Avenue, Hayes, Middlesex'
     provider: RoyalMailProvider = PROVIDER_REGISTER.get('ROYAL_MAIL')
     res = provider.client.address_search(search_text)
     dump_result_model(res)
-
-    ...
+    addr_id = res.addresses[0].address_id
+    retrieved = provider.client.address_retrieve(addr_id)
+    dump_result_model(retrieved)
