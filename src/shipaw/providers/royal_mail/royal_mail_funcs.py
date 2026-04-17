f27884539ef61a34bb74f506acdf4344a42b4d5a
Paw <paw@pawrequest.com>
2026-04-17 12:51:19 +0100

    shiping date at end of royal mail order reference

--- src/shipaw/providers/royal_mail/royal_mail_funcs.py
+++ src/shipaw/providers/royal_mail/royal_mail_funcs.py
@@ -91,7 +91,7 @@ def outbound_shipment_from_agnostic(shipment: Shipment, service_code: RoyalMailS
     postage_details = create_postage_details(shipment=shipment, service_code=service_code)
 
     return CreateOrderRequest(
-        order_reference=shipment.shipping_date.strftime('%d/%m') + ' ' + shipment.reference[0:34],
+        order_reference=shipment.reference[0:34] + ' ' * 5 + shipment.shipping_date.strftime('%d/%m'),
         postage_details=postage_details,
         billing=billing_details,
         recipient=rm_recipient_details_from_agnostic_fc(shipment.recipient),
