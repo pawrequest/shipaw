0ccc36ef6951793e956eb1d6e8b1a90e9c1ce5e5
Paw <paw@pawrequest.com>
2026-06-05 15:20:18 +0100

    use mobile phone2 in front end

--- src/shipaw/fapi/routes_html.py
+++ src/shipaw/fapi/routes_html.py
@@ -79,7 +79,7 @@ async def order_results(
 
 @router.get('/home_mobile_phone', response_class=HTMLResponse)
 async def home_mobile_phone(request: Request) -> HTMLResponse:
-    mobile_phone = request.app.shipaw_settings.mobile_phone
+    mobile_phone = request.app.shipaw_settings.mobile_phone2
     return HTMLResponse(f"""
     <input type="tel" id="mobile_phone" name="mobile_phone" value="{mobile_phone}" required>
     """)
