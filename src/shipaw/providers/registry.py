d6a216bccc8677f4e83ec803225f526af72a860f
Paw <paw@pawrequest.com>
2026-07-24 16:10:29 +0100

    change log level to debug for provider registration

--- src/shipaw/providers/registry.py
+++ src/shipaw/providers/registry.py
@@ -8,11 +8,11 @@ PROVIDER_REGISTER: dict[str, ShippingProvider] = {}
 
 def register_provider_type(cls):
     PROVIDER_TYPE_REGISTER[cls.name] = cls
-    logger.info(f'Registered provider type {cls.name}')
+    logger.debug(f'Registered provider type {cls.name}')
     return cls
 
 
 def register_provider_instance(instance: ShippingProvider):
     PROVIDER_REGISTER[instance.name] = instance
-    logger.info(f'Registered active provider instance {instance.name}')
+    logger.debug(f'Registered active provider instance {instance.name}')
     return instance
